27ff727fac1d6a2c1da042505232f20ef1164cd3
ndmanvar <ndmanvar@yahoo.com>
2016-10-11 12:58:50 -0700

    using https

--- src/test/java/com/yourcompany/saucecucumberjvm/GuineaPigSteps.java
+++ src/test/java/com/yourcompany/saucecucumberjvm/GuineaPigSteps.java
@@ -20,14 +20,14 @@ import java.net.URL;
 import Utils.SauceUtils;
 
 public class GuineaPigSteps {
-	
+
 	public static final String USERNAME = System.getenv("SAUCE_USERNAME");
 	public static final String ACCESS_KEY = System.getenv("SAUCE_ACCESS_KEY");
-	public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
+	public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
 	public static WebDriver driver;
 	public String sessionId;
 	public String jobName;
-	
+
 	@Before
 	public void setUp(Scenario scenario) throws Throwable {
         DesiredCapabilities caps = new DesiredCapabilities();
@@ -38,28 +38,28 @@ public class GuineaPigSteps {
 
 	    driver = new RemoteWebDriver(new URL(URL), caps);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        
+
         jobName = caps.getCapability("name").toString();
-        
+
         sessionId = (((RemoteWebDriver) driver).getSessionId()).toString();
 	}
-	
+
 	@Given("^I am on the Sauce Labs Guinea Pig test page$")
 	public void user_is_on_guinea_pig_page() throws Throwable {
         driver.get("https://saucelabs.com/test/guinea-pig");
 	}
- 
+
 	@When("^I click on the link$")
 	public void user_click_on_the_link() throws Throwable {
 		driver.findElement(By.linkText("i am a link")).click();
 	}
- 
+
 	@Then("^I should see a new page$")
 	public void new_page_displayed() throws Throwable {
 		String page_title = driver.getTitle();
 		Assert.assertTrue(page_title.equals("I am another page title - Sauce Labs"));
 	}
-	
+
 	@After
 	public void tearDown(Scenario scenario) throws Throwable {
 		driver.quit();
