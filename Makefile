f9be972611aea2c872cbfad75a5c6c9caa43a3ae
EX_Rago, Giovanni (Heidelberg) DEU <giovanni.rago@testobject.com>
2017-03-10 16:27:37 +0100

    TestObject setup

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 run_all_in_parallel:
-	make -j test_Windows8.1_firefox_40 test_Windows7_chrome_45
+	make -j test_LG_Nexus_4_E960_real test_Motorola_Moto_G_2nd_gen_real
 	
-test_Windows8.1_firefox_40:
-	browserName=firefox version=40.0 platform="Windows 8.1" mvn test
+test_LG_Nexus_4_E960_real:
+	deviceName=LG_Nexus_4_E960_real mvn test
 
-test_Windows7_chrome_45:
-	browserName=chrome version=45.0 platform="Windows 7" mvn test
+test_Motorola_Moto_G_2nd_gen_real:
+	deviceName=Motorola_Moto_G_2nd_gen_real mvn test
--- src/test/java/com/yourcompany/saucecucumberjvm/GuineaPigSteps.java
+++ src/test/java/com/yourcompany/saucecucumberjvm/GuineaPigSteps.java
@@ -1,47 +1,39 @@
 package com.yourcompany.saucecucumberjvm;
 
+import Utils.ResultReporter;
 import cucumber.api.Scenario;
-
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-
-import org.testng.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
 
-import java.util.concurrent.TimeUnit;
 import java.net.URL;
-
-import Utils.SauceUtils;
+import java.util.concurrent.TimeUnit;
 
 public class GuineaPigSteps {
 
-	public static final String USERNAME = System.getenv("SAUCE_USERNAME");
-	public static final String ACCESS_KEY = System.getenv("SAUCE_ACCESS_KEY");
-	public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
+	private ResultReporter resultReporter;
+
 	public static WebDriver driver;
-	public String sessionId;
-	public String jobName;
 
 	@Before
 	public void setUp(Scenario scenario) throws Throwable {
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("platform", System.getenv("platform"));
-        caps.setCapability("browserName", System.getenv("browserName"));
-        caps.setCapability("version", System.getenv("version"));
-        caps.setCapability("name", scenario.getName());
+        caps.setCapability("testobject_api_key", System.getenv("TESTOBJECT_API_KEY"));
+        caps.setCapability("testobject_device", System.getenv("deviceName"));
+		caps.setCapability("testobject_appium_version", System.getenv("TESTOBJECT_APPIUM_VERSION"));
 
-	    driver = new RemoteWebDriver(new URL(URL), caps);
+	    driver = new RemoteWebDriver(new URL("http://appium.testobject.com/wd/hub"), caps);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        jobName = caps.getCapability("name").toString();
+		resultReporter = new ResultReporter();
 
-        sessionId = (((RemoteWebDriver) driver).getSessionId()).toString();
 	}
 
 	@Given("^I am on the Sauce Labs Guinea Pig test page$")
@@ -62,8 +54,12 @@ public class GuineaPigSteps {
 
 	@After
 	public void tearDown(Scenario scenario) throws Throwable {
+
+		boolean success = !scenario.isFailed();
+
+		String sessionId = (((RemoteWebDriver) driver).getSessionId()).toString();
+		resultReporter.saveTestStatus(sessionId, success);
 		driver.quit();
-		SauceUtils.UpdateResults(USERNAME, ACCESS_KEY, !scenario.isFailed(), sessionId);
-		System.out.println("SauceOnDemandSessionID="+ sessionId + "job-name="+ jobName);
+
 	}
 }
