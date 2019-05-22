92cb40d0c8401fbd1537c9b30233353af584a846
James Tacker <james.tacker@saucelabs.com>
2019-05-22 13:58:25 -0700

    added parallel testing examples

---
+++ parallel-testing/cross-browser-and-platform-config/src/test/java/pages/SauceDemoNavigation.java
@@ -0,0 +1,24 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class SauceDemoNavigation {
+    private WebDriver driver;
+    public SauceDemoNavigation(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public LoginPage goToLoginPage() {
+        LoginPage loginPage = new LoginPage();
+        PageFactory.initElements(driver, loginPage);
+        driver.get(loginPage.getUrl());
+        return loginPage;
+    }
+
+    public InventoryPage getInventoryPage() {
+        InventoryPage inventoryPage = new InventoryPage();
+        PageFactory.initElements(driver, inventoryPage);
+        return inventoryPage;
+    }
+}
