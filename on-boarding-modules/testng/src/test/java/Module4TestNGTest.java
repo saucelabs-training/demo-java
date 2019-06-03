f2b2fc6c5c2f2978b8a9bfc38fdb594bcd33e4d0
James Tacker <45496368+spider-sauce@users.noreply.github.com>
2019-06-03 10:20:01 -0700

    W3C update to module4 (#18)
    * updated module4 with w3c

--- on-boarding-modules/testng/src/test/java/Module4TestNGTest.java
+++ on-boarding-modules/testng/src/test/java/Module4TestNGTest.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
@@ -30,38 +32,58 @@ public class Module4TestNGTest {
         String sauceURL = "https://ondemand.saucelabs.com/wd/hub";
 
         /**
-         * Here we set DesiredCapabilities, in this exercise we set additional capabilities below that align with
-         * testing best practices such as timeouts, tags, and build numbers
+         * * Here we set the MutableCapabilities for "sauce:options", which is required for newer versions of Selenium
+         * and the w3c protocol, for more info read the documentation:
+         * https://wiki.saucelabs.com/display/DOCS/Selenium+W3C+Capabilities+Support+-+Beta */
+        MutableCapabilities sauceOpts = new MutableCapabilities();
+        sauceOpts.setCapability("username", sauceUserName);
+        sauceOpts.setCapability("accessKey", sauceAccessKey);
+        /** In order to use w3c you must set the seleniumVersion **/
+        sauceOpts.setCapability("seleniumVersion", "3.141.59");
+        sauceOpts.setCapability("name", "4-best-practices");
+
+        /**
+         * in this exercise we set additional capabilities below that align with
+         * testing best practices such as tags, timeouts, and build name/numbers.
+         *
+         * Tags are an excellent way to control and filter your test automation
+         * in Sauce Analytics. Get a better view into your test automation.
          */
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("username", sauceUserName);
-        capabilities.setCapability("accessKey", sauceAccessKey);
-        capabilities.setCapability("browserName", "Chrome");
-        capabilities.setCapability("platform", "Windows 10");
-        capabilities.setCapability("version", "59.0");
-        capabilities.setCapability("name", "4-best-practices");
 
         /** Tags are an excellent way to control and filter your test automation
          * in Sauce Analytics. Get a better view into your test automation.
          */
         List<String> tags = Arrays.asList("sauceDemo", "demoTest", "module4");
-        capabilities.setCapability("tags", tags);
+        sauceOpts.setCapability("tags", tags);
 
         /** Another of the most important things that you can do to get started
          * is to set timeout capabilities for Sauce based on your organizations needs. For example:
          * How long is the whole test allowed to run?*/
-        capabilities.setCapability("maxDuration", 3600);
+        sauceOpts.setCapability("maxDuration", 3600);
         /** A Selenium crash might cause a session to hang indefinitely.
          * Below is the max time allowed to wait for a Selenium command*/
-        capabilities.setCapability("commandTimeout", 600);
+        sauceOpts.setCapability("commandTimeout", 600);
         /** How long can the browser wait for a new command */
-        capabilities.setCapability("idleTimeout", 1000);
+        sauceOpts.setCapability("idleTimeout", 1000);
 
         /** Setting a build name is one of the most fundamental pieces of running
          * successful test automation. Builds will gather all of your tests into a single
          * 'test suite' that you can analyze for results.
          * It's a best practice to always group your tests into builds. */
-        capabilities.setCapability("build", "Onboarding Sample App - Java-TestNG");
+        sauceOpts.setCapability("build", "Onboarding Sample App - Java-TestNG");
+
+
+        /** Required to set w3c protoccol **/
+        ChromeOptions chromeOpts = new ChromeOptions();
+        chromeOpts.setExperimentalOption("w3c", true);
+
+        /** Set a second MutableCapabilities object to pass Sauce Options and Chrome Options **/
+        MutableCapabilities capabilities = new MutableCapabilities();
+        capabilities.setCapability("sauce:options", sauceOpts);
+        capabilities.setCapability("goog:chromeOptions", chromeOpts);
+        capabilities.setCapability("browserName", "chrome");
+        capabilities.setCapability("platformVersion", "Windows 10");
+        capabilities.setCapability("browserVersion", "latest");
 
         /** If you're accessing the EU data center, use the following endpoint:.
          * https://ondemand.eu-central-1.saucelabs.com/wd/hub
