1bf78829bd5b63c57528a521fcdc7a3c03ed6b15
James Tacker <james.tacker@saucelabs.com>
2019-06-03 10:14:27 -0700

    updated module4 with w3c

--- on-boarding-modules/junit/src/test/java/Module4JunitTest.java
+++ on-boarding-modules/junit/src/test/java/Module4JunitTest.java
@@ -1,7 +1,9 @@
 import org.junit.jupiter.api.*;
 
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.MalformedURLException;
@@ -26,37 +28,50 @@ public class Module4JunitTest {
         String sauceAccessKey = System.getenv("SAUCE_ACCESS_KEY");
         String sauceURL = "https://ondemand.saucelabs.com/wd/hub";
         /**
-         * * Here we set DesiredCapabilities, in this exercise we set additional capabilities below that align with
-         * * testing best practices such as timeouts, tags, and build numbers
-         * */
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("username", sauceUserName);
-        capabilities.setCapability("accessKey", sauceAccessKey);
-        capabilities.setCapability("browserName", "Chrome");
-        capabilities.setCapability("platform", "Windows 10");
-        capabilities.setCapability("version", "59.0");
-        capabilities.setCapability("name", "4-best-practices");
-
-        /** Tags are an excellent way to control and filter your test automation
+         * * Here we set the MutableCapabilities for "sauce:options", which is required for newer versions of Selenium
+         * and the w3c protocol, for more info read the documentation:
+         * https://wiki.saucelabs.com/display/DOCS/Selenium+W3C+Capabilities+Support+-+Beta */
+        MutableCapabilities sauceOpts = new MutableCapabilities();
+        sauceOpts.setCapability("username", sauceUserName);
+        sauceOpts.setCapability("accessKey", sauceAccessKey);
+        sauceOpts.setCapability("name", "4-best-practices");
+        /**
+         * in this exercise we set additional capabilities below that align with
+         * testing best practices such as tags, timeouts, and build name/numbers.
+         *
+         * Tags are an excellent way to control and filter your test automation
          * in Sauce Analytics. Get a better view into your test automation.
          */
         List<String> tags = Arrays.asList("sauceDemo", "demoTest", "module4", "javaTest");
-        capabilities.setCapability("tags", tags);
+        sauceOpts.setCapability("tags", tags);
         /** Another of the most important things that you can do to get started
          * is to set timeout capabilities for Sauce based on your organizations needs. For example:
          * How long is the whole test allowed to run?*/
-        capabilities.setCapability("maxDuration", 3600);
+        sauceOpts.setCapability("maxDuration", 3600);
         /** A Selenium crash might cause a session to hang indefinitely.
          * Below is the max time allowed to wait for a Selenium command*/
-        capabilities.setCapability("commandTimeout", 600);
+        sauceOpts.setCapability("commandTimeout", 600);
         /** How long can the browser wait for a new command */
-        capabilities.setCapability("idleTimeout", 1000);
+        sauceOpts.setCapability("idleTimeout", 1000);
 
         /** Setting a build name is one of the most fundamental pieces of running
          * successful test automation. Builds will gather all of your tests into a single
          * 'test suite' that you can analyze for results.
          * It's a best practice to always group your tests into builds. */
-        capabilities.setCapability("build", "Onboarding Sample App - Java-Junit5");
+        sauceOpts.setCapability("build", "Onboarding Sample App - Java-Junit5");
+
+        /** Required to set w3c protoccol **/
+        ChromeOptions chromeOpts = new ChromeOptions();
+        chromeOpts.setExperimentalOption("w3c", true);
+
+        /** Set a second MutableCapabilities object to pass Sauce Options and Chrome Options **/
+        MutableCapabilities capabilities = new MutableCapabilities();
+        capabilities.setCapability("sauce:options", sauceOpts);
+        capabilities.setCapability("goog:chromeOptions", chromeOpts);
+        capabilities.setCapability("browserName", "chrome");
+        capabilities.setCapability("platformVersion", "Windows 10");
+        capabilities.setCapability("browserVersion", "latest");
+
 
         /** If you're accessing the EU data center, use the following endpoint:.
          * https://ondemand.eu-central-1.saucelabs.com/wd/hub
@@ -64,7 +79,7 @@ public class Module4JunitTest {
         driver = new RemoteWebDriver(new URL(sauceURL), capabilities);
         /** Don't forget to enter in your application's URL in place of 'https://www.saucedemo.com'. */
         driver.navigate().to("https://www.saucedemo.com");
-        //assertTrue(true);
+        assertTrue(true);
     }
 
     /**
