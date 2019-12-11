ce22aadcde80093f14f077085aeea9765a5c505a
joshgrant <joshua.m.grant@gmail.com>
2019-12-11 17:10:21 -0500

    starting out, starting strong

--- src/test/java/com/yourcompany/Tests/TestBase.java
+++ src/test/java/com/yourcompany/Tests/TestBase.java
@@ -2,6 +2,8 @@ package com.yourcompany.Tests;
 
 import com.saucelabs.common.SauceOnDemandAuthentication;
 
+import com.saucelabs.simplesauce.SauceOptions;
+import com.saucelabs.simplesauce.SauceSession;
 import org.junit.*;
 import org.junit.rules.TestName;
 import org.junit.runner.RunWith;
@@ -29,24 +31,7 @@ import com.saucelabs.common.SauceOnDemandSessionIdProvider;
  * @author Neil Manvar
  */
 @Ignore
-@RunWith(ConcurrentParameterized.class)
 public class TestBase implements SauceOnDemandSessionIdProvider {
-
-    public static String username = System.getenv("SAUCE_USERNAME");
-    public static String accesskey = System.getenv("SAUCE_ACCESS_KEY");
-    public static String seleniumURI;
-    public static String buildTag;
-    /**
-     * Constructs a {@link SauceOnDemandAuthentication} instance using the supplied user name/access key.  To use the authentication
-     * supplied by environment variables or from an external file, use the no-arg {@link SauceOnDemandAuthentication} constructor.
-     */
-    public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication(username, accesskey);
-
-    /**
-     * JUnit Rule which will mark the Sauce Job as passed/failed when the test succeeds or fails.
-     */
-    @Rule
-    public SauceOnDemandTestWatcher resultReportingTestWatcher = new SauceOnDemandTestWatcher(this, authentication);
 
     @Rule
     public TestName name = new TestName() {
@@ -55,84 +40,28 @@ public class TestBase implements SauceOnDemandSessionIdProvider {
         }
     };
 
-    protected String browser;
-    protected String os;
-    protected String version;
-    protected String deviceName;
-    protected String deviceOrientation;
+    protected SauceOptions options;
+    protected SauceSession sauce;
     protected String sessionId;
     protected WebDriver driver;
 
-    /**
-     * Constructs a new instance of the test.  The constructor requires three string parameters, which represent the operating
-     * system, version and browser to be used when launching a Sauce VM.  The order of the parameters should be the same
-     * as that of the elements within the {@link #browsersStrings()} method.
-     * @param os
-     * @param version
-     * @param browser
-     * @param deviceName
-     * @param deviceOrientation
-     */
-
-    public TestBase(String os, String version, String browser, String deviceName, String deviceOrientation) {
-        super();
-        this.os = os;
-        this.version = version;
-        this.browser = browser;
-        this.deviceName = deviceName;
-        this.deviceOrientation = deviceOrientation;
-    }
 
     /**
-     * @return a LinkedList containing String arrays representing the browser combinations the test should be run against. The values
-     * in the String array are used as part of the invocation of the test constructor
-     */
-    @ConcurrentParameterized.Parameters
-    public static LinkedList browsersStrings() {
-        LinkedList browsers = new LinkedList();
-
-        browsers.add(new String[]{"Windows 10", "14.14393", "MicrosoftEdge", null, null});
-        browsers.add(new String[]{"Windows 10", "49.0", "firefox", null, null});
-        browsers.add(new String[]{"Windows 7", "11.0", "internet explorer", null, null});
-        browsers.add(new String[]{"OS X 10.11", "10.0", "safari", null, null});
-        browsers.add(new String[]{"OS X 10.10", "54.0", "chrome", null, null});
-        return browsers;
-    }
-
-    /**
-     * Constructs a new {@link RemoteWebDriver} instance which is configured to use the capabilities defined by the {@link #browser},
-     * {@link #version} and {@link #os} instance variables, and which is configured to run against ondemand.saucelabs.com, using
-     * the username and access key populated by the {@link #authentication} instance.
+     * Construct a new session using the Sauce Java bindings
      *
-     * @throws Exception if an error occurs during the creation of the {@link RemoteWebDriver} instance.
      */
     @Before
-    public void setUp() throws Exception {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+    public void setUp() {
+        options = new SauceOptions();
+        sauce = new SauceSession(options);
 
-        capabilities.setCapability(CapabilityType.BROWSER_NAME, browser);
-        capabilities.setCapability(CapabilityType.VERSION, version);
-        capabilities.setCapability("deviceName", deviceName);
-        capabilities.setCapability("device-orientation", deviceOrientation);
-        capabilities.setCapability(CapabilityType.PLATFORM, os);
-
-        String methodName = name.getMethodName();
-        capabilities.setCapability("name", methodName);
-
-        //Getting the build name.
-        //Using the Jenkins ENV var. You can use your own. If it is not set test will run without a build id.
-        if (buildTag != null) {
-            capabilities.setCapability("build", buildTag);
-        }
-        this.driver = new RemoteWebDriver(
-                new URL("https://" + username+ ":" + accesskey + seleniumURI +"/wd/hub"),
-                capabilities);
+        driver = sauce.start();
 
         this.sessionId = (((RemoteWebDriver) driver).getSessionId()).toString();
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() {
         driver.quit();
     }
 
@@ -144,15 +73,4 @@ public class TestBase implements SauceOnDemandSessionIdProvider {
         return sessionId;
     }
 
-    @BeforeClass
-    public static void setupClass() {
-        //get the uri to send the commands to.
-        seleniumURI = "@ondemand.saucelabs.com:443";
-        //If available add build tag. When running under Jenkins BUILD_TAG is automatically set.
-        //You can set this manually on manual runs.
-        buildTag = System.getenv("BUILD_TAG");
-        if (buildTag == null) {
-            buildTag = System.getenv("SAUCE_BUILD_NAME");
-        }
-    }
 }
