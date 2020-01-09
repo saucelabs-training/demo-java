a5f2ed7d906ba5af7a01f2e571fbf85833872047
James Tacker <45496368+spider-sauce@users.noreply.github.com>
2020-01-08 19:02:25 -0800

    On boarding readme (#30)
    * added headless readme
    
    * added java on-boarding-module README

--- on-boarding-modules/testng/src/test/java/Module4TestNGTest.java
+++ on-boarding-modules/testng/src/test/java/Module4TestNGTest.java
@@ -2,14 +2,13 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Arrays;
@@ -19,14 +18,8 @@ public class Module4TestNGTest {
 
     private WebDriver driver;
 
-    @Test
-    public void shouldOpenChrome() throws MalformedURLException {
-        /** Here we set environment variables from your local machine, or IntelliJ run configuration,
-         *  and store these values in the variables below. Doing this is a best practice in terms of test execution
-         *  and security. If you're not sure how to use env variables, refer to this guide -
-         * https://wiki.saucelabs.com/display/DOCS/Best+Practice%3A+Use+Environment+Variables+for+Authentication+Credentials
-         * or check testng-README.md */
-
+    @BeforeMethod
+    public void setupTestMethod() throws MalformedURLException {
         String sauceUserName = System.getenv("SAUCE_USERNAME");
         String sauceAccessKey = System.getenv("SAUCE_ACCESS_KEY");
         String sauceURL = "https://ondemand.saucelabs.com/wd/hub";
@@ -89,15 +82,15 @@ public class Module4TestNGTest {
          * https://ondemand.eu-central-1.saucelabs.com/wd/hub
          * */
         driver = new RemoteWebDriver(new URL(sauceURL), capabilities);
+    }
+    @Test
+    public void shouldOpenChrome() {
         /** Don't forget to enter in your application's URL in place of 'https://www.saucedemo.com'. */
         driver.navigate().to("https://www.saucedemo.com");
         Assert.assertTrue(true);
 
     }
-    /**
-     * Below we are performing 2 critical actions. Quitting the driver and passing
-     * the test result to Sauce Labs user interface using the Javascript Executor.
-     */
+
     @AfterMethod
     public void cleanUpAfterTestMethod(ITestResult result) {
         ((JavascriptExecutor)driver).executeScript("sauce:job-result=" + (result.isSuccess() ? "passed" : "failed"));
