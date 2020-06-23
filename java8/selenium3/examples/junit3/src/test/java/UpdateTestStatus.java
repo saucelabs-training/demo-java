a67e4e804dcd71ea2eb61c5cec7d61b89af8835f
nadvolod <nadvolod@gmail.com>
2020-06-23 13:16:25 -0400

    better name

--- java8/selenium3/examples/junit3/src/test/java/UpdateTestStatus.java
+++ java8/selenium3/examples/junit3/src/test/java/UpdateTestStatus.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class SauceStatusTest extends TestCase {
+public class UpdateTestStatus extends TestCase {
     private WebDriver driver;
     private boolean isTestPassed = false;
 
