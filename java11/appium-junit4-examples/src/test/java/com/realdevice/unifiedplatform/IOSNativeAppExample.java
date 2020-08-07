304d0738ac8c2a9265d2ecebf49ea405d7d39b11
nadvolod <nadvolod@gmail.com>
2020-08-07 10:24:51 -0400

    add rdc ios test

---
+++ java11/appium-junit4-examples/src/test/java/com/realdevice/unifiedplatform/IOSNativeAppExample.java
@@ -0,0 +1,68 @@
+package com.realdevice.unifiedplatform;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.ios.IOSDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.openqa.selenium.By;
+import org.openqa.selenium.MutableCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.junit.Assert.assertTrue;
+
+public class IOSNativeAppExample {
+    @Rule
+    public TestName name = new TestName() {
+        public String getMethodName() {
+            return String.format("%s", super.getMethodName());
+        }
+    };
+    private AppiumDriver<MobileElement> driver;
+
+    public AppiumDriver<MobileElement> getDriver() {
+        return driver;
+    }
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        MutableCapabilities capabilities = new MutableCapabilities();
+        capabilities.setCapability("appiumVersion", "1.17.1");
+        capabilities.setCapability("idleTimeout", "90");
+        capabilities.setCapability("noReset", "true");
+        capabilities.setCapability("newCommandTimeout", "90");
+        capabilities.setCapability("language", "en");
+        capabilities.setCapability("platformName", "iOS");
+        capabilities.setCapability("platformVersion", "13.2");
+        capabilities.setCapability("deviceName", "iPhone 11 Pro Max");
+        capabilities.setCapability("name", name.getMethodName());
+        //You need to upload your own Native Mobile App to Sauce Storage!
+        //https://wiki.saucelabs.com/display/DOCS/Uploading+your+Application+to+Sauce+Storage
+//        capabilities.setCapability("app", "storage:filename=" + "SwagLabsMobileApp.ipa");
+        capabilities.setCapability("app", "storage:" + "1c17ee33-cdb4-405b-bc0c-050ef332cc1b");
+
+        driver = new IOSDriver(
+                new URL("https://" + System.getenv("SAUCE_USERNAME") + ":" +
+                        System.getenv("SAUCE_ACCESS_KEY") +
+                        "@ondemand.saucelabs.com:443" + "/wd/hub"),
+                capabilities);
+    }
+
+
+    @After
+    public void tearDown() {
+        if (getDriver() != null) {
+            getDriver().quit();
+        }
+    }
+
+    @Test
+    public void shouldOpenApp() {
+        assertTrue(getDriver().findElement(By.id("test-LOGIN")).isDisplayed());
+    }
+}
