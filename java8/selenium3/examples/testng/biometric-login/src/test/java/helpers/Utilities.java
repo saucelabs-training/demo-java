3c4c544d9a9bbb24504ba9a3edd093f46c27d490
Eyal Yovel <eyalyovel@gmail.com>
2020-08-30 21:29:21 +0100

    few changes 2

--- java8/selenium3/examples/testng/biometric-login/src/test/java/helpers/Utilities.java
+++ java8/selenium3/examples/testng/biometric-login/src/test/java/helpers/Utilities.java
@@ -3,7 +3,7 @@ package helpers;
 import java.io.*;
 import java.util.Properties;
 
-public class Utils {
+public class Utilities {
 
 
     public static String getProperty(String propertyName, String defaultVal){
