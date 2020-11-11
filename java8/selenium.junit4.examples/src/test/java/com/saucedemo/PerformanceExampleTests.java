4426c0c5a1a6e18b846f6e4cd9452982d25cc1bb
nadvolod <nadvolod@gmail.com>
2020-11-11 13:09:59 -0500

    trying to add custom perf test

--- java8/selenium.junit4.examples/src/test/java/com/saucedemo/PerformanceExampleTests.java
+++ java8/selenium.junit4.examples/src/test/java/com/saucedemo/PerformanceExampleTests.java
@@ -2,20 +2,50 @@ package com.saucedemo;
 
 import com.saucelabs.saucebindings.SauceOptions;
 import com.saucelabs.saucebindings.SauceSession;
+import org.junit.After;
 import org.junit.Test;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
+import java.util.HashMap;
+
 public class PerformanceExampleTests {
+
+    private SauceSession session;
+    private SauceOptions sauceOptions;
+
+    @After
+    public void tearDown() {
+        session.stop(true);
+    }
+
     @Test
     public void simplePerformanceTest() {
         //This is the easiest way to run a performance test in Sauce
-        SauceOptions sauceOptions = new SauceOptions();
+        //Let Sauce do all the work and keep track of the baseline
+        //and the regressions
+        sauceOptions = new SauceOptions();
         sauceOptions.setExtendedDebugging(true);
         sauceOptions.setCapturePerformance(true);
 
-        SauceSession session = new SauceSession(sauceOptions);
+        session = new SauceSession(sauceOptions);
         WebDriver driver = session.start();
         driver.get("https://www.saucedemo.com");
-        session.stop(true);
+    }
+
+    @Test
+    public void specificMetricsTests() {
+        //If you want, you can assert on specific metrics
+        sauceOptions = new SauceOptions();
+        sauceOptions.setExtendedDebugging(true);
+        sauceOptions.setCapturePerformance(true);
+        session = new SauceSession(sauceOptions);
+
+        HashMap<Object, Object> metrics = new HashMap<>();
+        metrics.put("type", "sauce:performance");
+        WebDriver driver = session.start();
+        driver.get("https://www.saucedemo.com");
+
+        HashMap perfMetrics = (HashMap<Object, Object>) ((JavascriptExecutor)driver).executeScript("sauce:log", metrics);
     }
 }
