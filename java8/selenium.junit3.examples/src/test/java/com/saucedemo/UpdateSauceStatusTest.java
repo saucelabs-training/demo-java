027ca25e85cedacc7acc529ffc0775e89bde48e5
nadvolod <nadvolod@gmail.com>
2020-11-12 09:09:16 -0500

    add sauce bindings

--- java8/selenium.junit3.examples/src/test/java/com/saucedemo/UpdateSauceStatusTest.java
+++ java8/selenium.junit3.examples/src/test/java/com/saucedemo/UpdateSauceStatusTest.java
@@ -1,46 +1,22 @@
 package com.saucedemo;
 
+import com.saucelabs.saucebindings.SauceSession;
 import junit.framework.TestCase;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class UpdateSauceStatusTest extends TestCase {
     private WebDriver driver;
     private boolean isTestPassed = false;
+    private SauceSession session;
 
-    //without sauce bindings
-    public void setUp() throws MalformedURLException {
-        String username = System.getenv("SAUCE_USERNAME");
-        String accessKey = System.getenv("SAUCE_ACCESS_KEY");
-
-        ChromeOptions chromeOpts = new ChromeOptions();
-        chromeOpts.setExperimentalOption("w3c", true);
-
-        MutableCapabilities sauceOpts = new MutableCapabilities();
-        sauceOpts.setCapability("username", username);
-        sauceOpts.setCapability("accessKey", accessKey);
-
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability(ChromeOptions.CAPABILITY,  chromeOpts);
-        caps.setCapability("sauce:options", sauceOpts);
-        caps.setCapability("browserName", "googlechrome");
-        caps.setCapability("browserVersion", "latest");
-        caps.setCapability("platformName", "windows 10");
-
-        String sauceUrl = "https://ondemand.saucelabs.com:443/wd/hub";
-        URL url = new URL(sauceUrl);
-        driver = new RemoteWebDriver(url, caps);
+    //with sauce bindings
+    public void setUp() {
+        session = new SauceSession();
+        driver = session.start();
     }
+
     public void tearDown() {
-        ((JavascriptExecutor) driver).executeScript("sauce:job-result=" + isTestPassed);
-        driver.quit();
+        session.stop(isTestPassed);
     }
     public void testSauceStatus() {
         driver.navigate().to("https://www.saucedemo.com");
