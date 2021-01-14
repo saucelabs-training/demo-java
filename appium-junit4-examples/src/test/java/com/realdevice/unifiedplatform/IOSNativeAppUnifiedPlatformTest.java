2e04aa41e6f91583e5c8bff7807971bef0bc5a23
nadvolod <nadvolod@gmail.com>
2021-01-14 09:33:59 -0500

    update test to pass

--- appium-junit4-examples/src/test/java/com/realdevice/unifiedplatform/IOSNativeAppUnifiedPlatformTest.java
+++ appium-junit4-examples/src/test/java/com/realdevice/unifiedplatform/IOSNativeAppUnifiedPlatformTest.java
@@ -32,17 +32,18 @@ public class IOSNativeAppUnifiedPlatformTest {
     @Before
     public void setUp() throws MalformedURLException {
         MutableCapabilities capabilities = new MutableCapabilities();
-        capabilities.setCapability("appiumVersion", "1.17.1");
         capabilities.setCapability("idleTimeout", "90");
         capabilities.setCapability("noReset", "true");
         capabilities.setCapability("newCommandTimeout", "90");
         capabilities.setCapability("language", "en");
         capabilities.setCapability("platformName", "iOS");
-        capabilities.setCapability("platformVersion", "13.2");
-        capabilities.setCapability("deviceName", "iPhone 11 Pro Max");
+        //Not specifying platformVersion or the exact device is the most likely to
+        //find a device in the cloud
+        capabilities.setCapability("deviceName", "iPhone.*");
         capabilities.setCapability("name", name.getMethodName());
 
-        capabilities.setCapability("app", "storage:filename=" + "iOS.RealDevice.SauceLabs.Mobile.Sample.app.2.3.0.ipa");
+        capabilities.setCapability("app", "storage:filename=" +
+                "iOS.RealDevice.Sample.ipa");
 
         driver = new IOSDriver(
                 new URL("https://" + System.getenv("SAUCE_USERNAME") + ":" +
