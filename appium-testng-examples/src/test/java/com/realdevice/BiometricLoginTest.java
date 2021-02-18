644d112e321de3a6cc33e99a96ed480d108524b7
Nikolay Advolodkin <nadvolod@gmail.com>
2021-02-18 09:28:46 -0500

    add appium testng examples

---
+++ appium-testng-examples/src/test/java/com/realdevice/BiometricLoginTest.java
@@ -0,0 +1,10 @@
+package com.realdevice;
+import org.testng.*;
+import org.testng.annotations.Test;
+
+public class BiometricLoginTest {
+    @Test
+    public void testBiometricLogin() {
+        //code for that goes here
+    }
+}
---
+++ appium-testng-examples/src/test/java/com/realdevice/ImageInjectionTest.java
@@ -0,0 +1,9 @@
+package com.realdevice;
+import org.testng.annotations.Test;
+
+public class ImageInjectionTest {
+    @Test
+    public void testImageInjection() {
+        //code for that goes here
+    }
+}
