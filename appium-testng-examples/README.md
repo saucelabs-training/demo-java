eda605be2b4d6d0fc152ae64fa6fb517256dbe3b
eyaly <eyalyovel@gmail.com>
2021-03-03 17:38:14 +0000

    Add image injection feature to new Appium structure (#73)
    * Add image injection feature
    
    * Add features and structures
    
    * Amend readme
    
    * Remove biometric-login and image-injection modules
    
    * pom changes
    
    * fix the readme
    
    * Change function names
    
    * amend the import
    
    * amend imports
    
    * Fix class names
    
    * few changes in names
    
    * change config file to constants
    
    * remove appname varriable
    
    * Remove java11/testng selenium framework
    
    Co-authored-by: nadvolod <nadvolod@gmail.com>

---
+++ appium-testng-examples/README.md
@@ -0,0 +1,6 @@
+# Appium with TestNG framework examples
+This folder contains Appium examples running with TestNG framework
+
+## Examples
+- [Using biometric login on Sauce Labs](./src/test/java/biometric_login)
+- [Using image injection on Sauce Labs](./src/test/java/image_injection)
---
+++ appium-testng-examples/src/test/java/helpers/Constants.java
@@ -0,0 +1,8 @@
+package helpers;
+
+public class Constants {
+    public static final String region = System.getProperty("region", "eu");
+    public static final String host = System.getProperty("host", "saucelabs");
+    public static final String rdc = System.getProperty("rdc", "true");
+
+}
