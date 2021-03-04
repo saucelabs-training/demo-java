52f18ed7aeb71132b09d61baf23d651935b0bd6a
nadvolod <nadvolod@gmail.com>
2021-03-04 12:55:04 -0500

    add massive parallelization to desk and emusim

--- best-practice/src/test/java/com/saucedemo/tests/EmusimWebAppTests.java
+++ best-practice/src/test/java/com/saucedemo/tests/EmusimWebAppTests.java
@@ -9,42 +9,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.util.Arrays;
 import java.util.Collection;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(Parameterized.class)
 public class EmusimWebAppTests extends WebTestsBase {
-
-    /*
-     * Configure our data driven parameters
-     * */
-    @Parameterized.Parameter
-    public String browserName;
-
-    @Parameterized.Parameter(1)
-    public String platform;
-
-    @Parameterized.Parameter(2)
-    public String platformVersion;
-
-    @Parameterized.Parameter(3)
-    public String deviceName;
-
-    @Parameterized.Parameters()
-    public static Collection<Object[]> crossBrowserData() {
-        return Arrays.asList(new Object[][] {
-                { "Safari", "iOS", "13.4", "iPhone XS Max Simulator" },
-                { "Safari", "iOS", "13.4", "iPhone XS Max Simulator" },
-                { "Safari", "iOS", "13.4", "iPhone XS Max Simulator" },
-                { "Safari", "iOS", "13.4", "iPhone XS Max Simulator" },
-                { "Safari", "iOS", "13.4", "iPhone XS Max Simulator" },
-        });
-    }
 
     @Before
     public void setUp() throws MalformedURLException {
@@ -73,5 +49,53 @@ public class EmusimWebAppTests extends WebTestsBase {
         loginPage.visit();
         loginPage.login("standard_user");
         assertTrue(new ProductsPage(driver).isDisplayed());
+    }
+    @Test(expected = TimeoutException.class)
+    public void lockedOutUser() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.visit();
+        loginPage.login("locked_out_user");
+        assertFalse(new ProductsPage(driver).isDisplayed());
+    }
+    @Test(expected = TimeoutException.class)
+    public void invalidCredentials() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.visit();
+        loginPage.login("foo_bar_user");
+        assertFalse(new ProductsPage(driver).isDisplayed());
+    }
+
+    /*
+     * Configure our data driven parameters
+     * */
+    @Parameterized.Parameter
+    public String browserName;
+
+    @Parameterized.Parameter(1)
+    public String platform;
+
+    @Parameterized.Parameter(2)
+    public String platformVersion;
+
+    @Parameterized.Parameter(3)
+    public String deviceName;
+
+    @Parameterized.Parameters()
+    public static Collection<Object[]> crossBrowserData() {
+        return Arrays.asList(new Object[][] {
+                { "Safari", "iOS", "14.3", "iPhone XS Max Simulator" },
+                { "Safari", "iOS", "14.3", "iPhone XS Simulator" },
+                { "Safari", "iOS", "14.3", "iPhone SE (2nd generation) Simulator" },
+                // Duplication below for demo purposes of massive parallelization
+                { "Safari", "iOS", "14.3", "iPhone XS Max Simulator" },
+                { "Safari", "iOS", "14.3", "iPhone XS Simulator" },
+                { "Safari", "iOS", "14.3", "iPhone SE (2nd generation) Simulator" },
+                { "Safari", "iOS", "14.3", "iPhone XS Max Simulator" },
+                { "Safari", "iOS", "14.3", "iPhone XS Simulator" },
+                { "Safari", "iOS", "14.3", "iPhone SE (2nd generation) Simulator" },
+                { "Safari", "iOS", "14.3", "iPhone XS Max Simulator" },
+                { "Safari", "iOS", "14.3", "iPhone XS Simulator" },
+                { "Safari", "iOS", "14.3", "iPhone SE (2nd generation) Simulator" },
+        });
     }
 }
