b89813f5c3866b9ea7506ee2617906d0455e0c0a
nadvolod <nadvolod@gmail.com>
2021-04-05 13:30:25 -0400

    adding selenium-examples module

--- selenium-cucumber-examples/cucumber/src/test/java/com/yourcompany/saucecucumberjvm/StepDefinitions.java
+++ selenium-cucumber-examples/cucumber/src/test/java/com/yourcompany/saucecucumberjvm/StepDefinitions.java
@@ -16,6 +16,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.util.stream.IntStream;
 
 public class StepDefinitions {
