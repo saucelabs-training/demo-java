bf0f87ac54ab8faafb275ecab11619464e1674f8
Evelyn Coleman <coleve27@gmail.com>
2021-04-08 08:39:09 -0500

    changed headless to use sauce bindings

--- selenium-examples/src/test/java/com/saucedemo/SampleHeadlessSauceTest.java
+++ selenium-examples/src/test/java/com/saucedemo/SampleHeadlessSauceTest.java
@@ -1,13 +1,11 @@
 package com.saucedemo;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import java.net.MalformedURLException;
-import java.net.URL;
+import com.saucelabs.saucebindings.DataCenter;
+import com.saucelabs.saucebindings.SauceOptions;
+import com.saucelabs.saucebindings.SaucePlatform;
+import com.saucelabs.saucebindings.SauceSession;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.MutableCapabilities;
-import org.openqa.selenium.chrome.ChromeOptions;
-
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
@@ -15,42 +13,29 @@ import org.testng.annotations.Test;
 
 public class SampleHeadlessSauceTest {
     private WebDriver driver;
+    private SauceSession sauceSession;
 
     @Test
-    public void main() throws MalformedURLException {
-        String sauceUserName = System.getenv("SAUCE_USERNAME");
-        String sauceAccessKey = System.getenv("SAUCE_ACCESS_KEY");
-        String URL = "https://ondemand.us-east-1.saucelabs.com/wd/hub";
+    public void main()  {
 
-        ChromeOptions chromeOpts = new ChromeOptions();
-        chromeOpts.setExperimentalOption("w3c", true);
+        SauceOptions sauceOptions = new SauceOptions();
+        sauceOptions.setPlatformName(SaucePlatform.LINUX);
+        sauceOptions.setName("headless-chrome-test-java");
+        sauceOptions.setBuild("Sample Headless Test");
 
-        MutableCapabilities sauceOptions = new MutableCapabilities();
-        sauceOptions.setCapability("username", sauceUserName);
-        sauceOptions.setCapability("accessKey", sauceAccessKey);
-        sauceOptions.setCapability("seleniumVersion", "3.141.59");
-        sauceOptions.setCapability("name", "headless-chrome-test-java");
-        sauceOptions.setCapability("build", "Sample Headless Tests");
-
-        MutableCapabilities caps = new MutableCapabilities();
-        caps.setCapability("goog:chromeOptions", chromeOpts);
-        caps.setCapability("browserName", "chrome");
-        caps.setCapability("browserVersion", "latest");
-        caps.setCapability("platformName", "Linux");
-        caps.setCapability("sauce:options", sauceOptions);
-
-        driver = new RemoteWebDriver(new URL(URL), caps);
-
-        /* Goes to Sauce Lab's demo page and prints title */
+        sauceSession = new SauceSession(sauceOptions);
+        sauceSession.setDataCenter(DataCenter.US_EAST);
+        driver = sauceSession.start();
 
         driver.get("https://www.saucedemo.com");
         System.out.println("title of page is: " + driver.getTitle());
-        Assert.assertEquals(driver.getTitle(), "Swag Labs" );
+        Assert.assertEquals(driver.getTitle(), "Swag Labs");
     }
+
     /* Sends results to SauceLabs.com */
     @AfterMethod
     public void cleanUpAfterTestMethod(ITestResult result) {
-        ((JavascriptExecutor)driver).executeScript("sauce:job-result=" + (result.isSuccess() ? "passed" : "failed"));
-        driver.quit();
+        ((JavascriptExecutor) driver).executeScript("sauce:job-result=" + (result.isSuccess() ? "passed" : "failed"));
+        sauceSession.stop(true);
     }
 }
