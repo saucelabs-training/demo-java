a3360374107c6b1d767b35ea095cccf5852720c5
titusfortner <titusfortner@gmail.com>
2021-07-15 21:08:00 -0500

    update accessibility tests to use saucebindings-junit5

--- selenium-examples/src/test/java/com/saucedemo/accessibility/SauceBindingsExampleTest.java
+++ selenium-examples/src/test/java/com/saucedemo/accessibility/SauceBindingsExampleTest.java
@@ -1,16 +1,19 @@
 package com.saucedemo.accessibility;
 
-import com.deque.html.axecore.results.Results;
-import com.saucelabs.saucebindings.junit4.SauceBaseTest;
-import org.junit.Assert;
-import org.junit.Test;
+import com.saucelabs.saucebindings.junit5.SauceBaseTest;
+import org.junit.jupiter.api.Test;
 
 public class SauceBindingsExampleTest extends SauceBaseTest {
 
     @Test
-    public void accessibilityTest() {
+    public void sauceDemoAccessibility() {
         driver.navigate().to("https://www.saucedemo.com");
-        Results accessibilityResults = session.getAccessibilityResults();
-        Assert.assertEquals(3, accessibilityResults.getViolations().size());
+        session.getAccessibilityResults();
+    }
+
+    @Test
+    public void abcdcomputechTest() {
+        driver.navigate().to("http://abcdcomputech.dequecloud.com");
+        session.getAccessibilityResults();
     }
 }
