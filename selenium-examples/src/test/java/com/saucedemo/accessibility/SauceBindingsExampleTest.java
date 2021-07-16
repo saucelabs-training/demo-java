013557e5acbb24142436491faefa0bc2ea4d7ff5
titusfortner <titusfortner@gmail.com>
2021-07-16 14:54:18 -0500

    update accessibility examples for demo

--- selenium-examples/src/test/java/com/saucedemo/accessibility/SauceBindingsExampleTest.java
+++ selenium-examples/src/test/java/com/saucedemo/accessibility/SauceBindingsExampleTest.java
@@ -1,6 +1,8 @@
 package com.saucedemo.accessibility;
 
+import com.deque.html.axecore.results.Results;
 import com.saucelabs.saucebindings.junit5.SauceBaseTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SauceBindingsExampleTest extends SauceBaseTest {
@@ -15,5 +17,12 @@ public class SauceBindingsExampleTest extends SauceBaseTest {
     public void abcdcomputechTest() {
         driver.navigate().to("http://abcdcomputech.dequecloud.com");
         session.getAccessibilityResults();
+    }
+
+    @Test
+    public void codeResultTest() {
+        driver.navigate().to("https://www.saucedemo.com");
+        Results results = session.getAccessibilityResults();
+        Assertions.assertEquals(3, results.getViolations().size());
     }
 }
