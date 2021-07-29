255f74dae662b2e3b3d966e0c7e19ea1595e60b0
nadvolod <nadvolod@gmail.com>
2021-07-29 13:09:32 -0400

    remove buildName

--- best-practice/src/test/java/com/saucedemo/tests/VisualCrossPlatformTests.java
+++ best-practice/src/test/java/com/saucedemo/tests/VisualCrossPlatformTests.java
@@ -58,7 +58,6 @@ public class VisualCrossPlatformTests extends WebTestsBase {
         MutableCapabilities sauceOptions = new MutableCapabilities();
         sauceOptions.setCapability("username", sauceUsername);
         sauceOptions.setCapability("accesskey", sauceAccessKey);
-        sauceOptions.setCapability("build", buildName);
         browserOptions.setCapability("sauce:options", sauceOptions);
 
         MutableCapabilities visualOptions = new MutableCapabilities();
