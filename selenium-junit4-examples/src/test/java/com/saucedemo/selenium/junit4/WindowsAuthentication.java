1855112f2cb0a4f9ece393f9d2fa600949392a66
titusfortner <titusfortner@gmail.com>
2021-08-12 18:24:37 -0500

    get everything passing

--- selenium-junit4-examples/src/test/java/com/saucedemo/selenium/junit4/WindowsAuthentication.java
+++ selenium-junit4-examples/src/test/java/com/saucedemo/selenium/junit4/WindowsAuthentication.java
@@ -15,6 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * Tests for Basic Authentication.
@@ -59,5 +60,7 @@ public class WindowsAuthentication {
         session = new SauceSession(sauceOptions);
         RemoteWebDriver driver = session.start();
         driver.get("http://the-internet.herokuapp.com/basic_auth");
+
+        fail();
     }
 }
