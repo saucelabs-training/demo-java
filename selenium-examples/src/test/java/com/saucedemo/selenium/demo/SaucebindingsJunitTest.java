cd7f66b127c816c7c6772a894d16fdcf0113b2a5
titusfortner <titusfortner@gmail.com>
2021-08-20 11:48:03 -0700

    update references to sauce bindings to reflect the new directory structure

--- selenium-examples/src/test/java/com/saucedemo/selenium/demo/SaucebindingsJunitTest.java
+++ selenium-examples/src/test/java/com/saucedemo/selenium/demo/SaucebindingsJunitTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 /**
  * saucebindings.junit5 package provides a default superclass to handle set up and tear down
  *
- * @see <a href="https://opensource.saucelabs.com/sauce_bindings/docs/test-runners/">Sauce Binding Test Runners</a>
+ * @see <a href="https://opensource.saucelabs.com/sauce_bindings/test-runners/">Sauce Binding Test Runners</a>
  *     for more information
  */
 public class SaucebindingsJunitTest extends SauceBaseTest {
