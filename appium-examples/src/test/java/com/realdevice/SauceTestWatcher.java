5d94cc6439e8ba836aecf465a026339487b53f51
Eyal Yovel <eyalyovel@gmail.com>
2022-02-03 13:15:37 +0000

    Amend the tests to support W3C capabilities

--- appium-examples/src/test/java/com/realdevice/SauceTestWatcher.java
+++ appium-examples/src/test/java/com/realdevice/SauceTestWatcher.java
@@ -1,5 +1,6 @@
 package com.realdevice;
 
+import io.appium.java_client.AppiumDriver;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -7,22 +8,26 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 public class SauceTestWatcher extends TestWatcher {
     private RemoteWebDriver driver;
 
-    public void setDriver(RemoteWebDriver driver)
+    public void setDriver(AppiumDriver driver)
     {
         this.driver = driver;
     }
 
+    @Override
     protected void succeeded(Description description) {
         if(driver != null)
         {
+            System.out.println("Test Passed!");
             driver.executeScript("sauce:job-result=passed");
             driver.quit();
         }
     }
 
-    protected void failed(Description description) {
+    @Override
+    public void failed(Throwable e, Description description) {
         if(driver != null)
         {
+            System.out.println("Test Failed!");
             driver.executeScript("sauce:job-result=failed");
             driver.quit();
         }
