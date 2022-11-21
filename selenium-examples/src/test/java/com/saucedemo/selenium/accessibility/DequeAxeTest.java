0a9446b64daf7ef100a1ed3d8bb5f28a91376331
titusfortner <titusfortner@gmail.com>
2022-11-21 13:18:26 -0600

    implement base class for Selenium-examples tests

--- selenium-examples/src/test/java/com/saucedemo/selenium/accessibility/DequeAxeTest.java
+++ selenium-examples/src/test/java/com/saucedemo/selenium/accessibility/DequeAxeTest.java
@@ -2,68 +2,31 @@ package com.saucedemo.selenium.accessibility;
 
 import com.deque.html.axecore.results.Results;
 import com.deque.html.axecore.selenium.AxeBuilder;
+import com.saucedemo.selenium.SeleniumTestBase;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.api.extension.RegisterExtension;
-import org.junit.jupiter.api.extension.TestWatcher;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Accessibility Tests with Deque Library.
  */
-public class DequeAxeTest {
-    public RemoteWebDriver driver;
-
-    @RegisterExtension
-    public SauceTestWatcher watcher = new SauceTestWatcher();
+public class DequeAxeTest extends SeleniumTestBase {
 
     @BeforeEach
-    public void setup(TestInfo testInfo) throws MalformedURLException {
-        ChromeOptions options = new ChromeOptions();
-        options.setPlatformName("Windows 10");
-        options.setBrowserVersion("latest");
-
-        Map<String, Object> sauceOptions = new HashMap<>();
-        sauceOptions.put("username", System.getenv("SAUCE_USERNAME"));
-        sauceOptions.put("accessKey", System.getenv("SAUCE_ACCESS_KEY"));
-        sauceOptions.put("name", testInfo.getDisplayName());
-
-        options.setCapability("sauce:options", sauceOptions);
-        URL url = new URL("https://ondemand.us-west-1.saucelabs.com/wd/hub");
-
-        driver = new RemoteWebDriver(url, options);
+    public void setup(TestInfo testInfo) {
+        basicSetup(testInfo);
     }
 
-    @DisplayName("Deque Axe Test With Selenium")
+    @DisplayName("Deque Axe Test With Selenium Not html")
     @Test
     public void accessibilityTest() {
         driver.navigate().to("https://www.saucedemo.com");
+
         AxeBuilder axeBuilder = new AxeBuilder();
         Results accessibilityResults = axeBuilder.analyze(driver);
+
         Assertions.assertEquals(3, accessibilityResults.getViolations().size());
-    }
-
-    public class SauceTestWatcher implements TestWatcher {
-        @Override
-        public void testSuccessful(ExtensionContext context) {
-            driver.executeScript("sauce:job-result=passed");
-            driver.quit();
-        }
-
-        @Override
-        public void testFailed(ExtensionContext context, Throwable cause) {
-            driver.executeScript("sauce:job-result=failed");
-            driver.quit();
-        }
     }
 }
--- selenium-examples/src/test/java/com/saucedemo/selenium/login/SeleniumLoginTest.java
+++ selenium-examples/src/test/java/com/saucedemo/selenium/login/SeleniumLoginTest.java
@@ -1,49 +1,24 @@
 package com.saucedemo.selenium.login;
 
+import com.saucedemo.selenium.SeleniumTestBase;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.api.extension.RegisterExtension;
-import org.junit.jupiter.api.extension.TestWatcher;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Login tests with Selenium.
  */
-public class SeleniumLoginTest {
-    protected RemoteWebDriver driver;
-
-    @RegisterExtension
-    public SauceTestWatcher watcher = new SauceTestWatcher();
-
+public class SeleniumLoginTest extends SeleniumTestBase {
     @BeforeEach
-    public void setup(TestInfo testInfo) throws MalformedURLException {
-        ChromeOptions options = new ChromeOptions();
-        options.setPlatformName("Windows 10");
-        options.setBrowserVersion("latest");
-
-        Map<String, Object> sauceOptions = new HashMap<>();
-        sauceOptions.put("username", System.getenv("SAUCE_USERNAME"));
-        sauceOptions.put("accessKey", System.getenv("SAUCE_ACCESS_KEY"));
-        sauceOptions.put("name", testInfo.getDisplayName());
-
-        options.setCapability("sauce:options", sauceOptions);
-        URL url = new URL("https://ondemand.us-west-1.saucelabs.com/wd/hub");
-
-        driver = new RemoteWebDriver(url, options);
+    public void setup(TestInfo testInfo) {
+        basicSetup(testInfo);
     }
 
     @DisplayName("Swag Labs Login with Selenium")
@@ -67,27 +42,5 @@ public class SeleniumLoginTest {
         submitButton.click();
 
         Assertions.assertEquals("https://www.saucedemo.com/inventory.html", driver.getCurrentUrl());
-    }
-
-    /**
-     * Custom TestWatcher for Sauce Labs projects.
-     */
-    public class SauceTestWatcher implements TestWatcher {
-        @Override
-        public void testSuccessful(ExtensionContext context) {
-            endSession(true);
-        }
-
-        @Override
-        public void testFailed(ExtensionContext context, Throwable cause) {
-            endSession(false);
-        }
-
-        private void endSession(boolean passed) {
-            String result = passed ? "passed" : "failed"; 
-            driver.executeScript("sauce:job-result=" + result);
-
-            driver.quit();
-        }
     }
 }
