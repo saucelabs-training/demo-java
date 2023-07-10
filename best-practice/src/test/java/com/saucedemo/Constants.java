b3a7f26d92b5970e61e1e88d4fc0988ee1a8c9e1
eyaly <eyal.yovel@saucelabs.com>
2023-07-10 11:11:26 +0100

    Fix best practice desktop tests (#154)
    * Amend the DesktopTests
    
    * Update Constants.java

--- best-practice/src/test/java/com/saucedemo/Constants.java
+++ best-practice/src/test/java/com/saucedemo/Constants.java
@@ -1,8 +1,7 @@
 package com.saucedemo;
 
 public class Constants {
-    public static final String region = System.getProperty("region", "eu");
-
+    public static final String region = System.getProperty("region", "us");
     public static final String SAUCE_EU_URL = "https://ondemand.eu-central-1.saucelabs.com:443/wd/hub";
     public static final String SAUCE_US_URL = "https://ondemand.us-west-1.saucelabs.com:443/wd/hub";
 
