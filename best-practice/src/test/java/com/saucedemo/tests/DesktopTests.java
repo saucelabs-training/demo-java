440e11ebc9e807cc1abc3044d8a1692900b7e2f1
eyaly <eyal.yovel@saucelabs.com>
2023-07-10 10:59:48 +0100

    Amend the DesktopTests (#153)

--- best-practice/src/test/java/com/saucedemo/tests/DesktopTests.java
+++ best-practice/src/test/java/com/saucedemo/tests/DesktopTests.java
@@ -2,76 +2,17 @@ package com.saucedemo.tests;
 
 import com.saucedemo.pages.LoginPage;
 import com.saucedemo.pages.ProductsPage;
-import com.saucelabs.saucebindings.Browser;
-import com.saucelabs.saucebindings.SaucePlatform;
-import com.saucelabs.saucebindings.junit4.SauceBaseTest;
-import com.saucelabs.saucebindings.options.SauceOptions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.openqa.selenium.TimeoutException;
-
-import java.util.Arrays;
-import java.util.Collection;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /** Desktop Tests. */
 @RunWith(Parameterized.class)
-public class DesktopTests extends SauceBaseTest {
-    /*
-     * Configure our data driven parameters
-     * */
-    @Parameterized.Parameter
-    public Browser browserName;
-    @Parameterized.Parameter(1)
-    public String browserVersion;
-    @Parameterized.Parameter(2)
-    public SaucePlatform platform;
-
-    @Parameterized.Parameters()
-    public static Collection<Object[]> crossBrowserData() {
-        return Arrays.asList(new Object[][] {
-                { Browser.CHROME, "latest", SaucePlatform.WINDOWS_10 },
-                { Browser.CHROME, "latest-1", SaucePlatform.WINDOWS_10 },
-                { Browser.SAFARI, "latest", SaucePlatform.MAC_MOJAVE },
-                { Browser.CHROME, "latest", SaucePlatform.MAC_MOJAVE }
-                /*
-                         // Duplication below for demo purposes of massive parallelization
-                         {Browser.CHROME, "latest", SaucePlatform.WINDOWS_10},
-                         {Browser.CHROME, "latest-1", SaucePlatform.WINDOWS_10},
-                         {Browser.SAFARI, "latest", SaucePlatform.MAC_MOJAVE},
-                         {Browser.CHROME, "latest", SaucePlatform.MAC_MOJAVE},
-                         {Browser.CHROME, "latest", SaucePlatform.WINDOWS_10},
-                         {Browser.CHROME, "latest-1", SaucePlatform.WINDOWS_10},
-                         {Browser.SAFARI, "latest", SaucePlatform.MAC_MOJAVE},
-                         {Browser.CHROME, "latest", SaucePlatform.MAC_MOJAVE},
-                         {Browser.CHROME, "latest", SaucePlatform.WINDOWS_10},
-                         {Browser.CHROME, "latest-1", SaucePlatform.WINDOWS_10},
-                         {Browser.SAFARI, "latest", SaucePlatform.MAC_MOJAVE},
-                         {Browser.CHROME, "latest", SaucePlatform.MAC_MOJAVE},
-                         {Browser.CHROME, "latest", SaucePlatform.WINDOWS_10},
-                         {Browser.CHROME, "latest-1", SaucePlatform.WINDOWS_10},
-                         {Browser.SAFARI, "latest", SaucePlatform.MAC_MOJAVE},
-                         {Browser.CHROME, "latest", SaucePlatform.MAC_MOJAVE},
-                         {Browser.CHROME, "latest", SaucePlatform.WINDOWS_10},
-                         {Browser.CHROME, "latest-1", SaucePlatform.WINDOWS_10},
-                         {Browser.SAFARI, "latest", SaucePlatform.MAC_MOJAVE},
-                         {Browser.CHROME, "latest", SaucePlatform.MAC_MOJAVE},
-                */
-        });
-    }
-
-    @Override
-    public SauceOptions createSauceOptions() {
-        SauceOptions sauceOptions = new SauceOptions();
-        sauceOptions.setBrowserName(browserName);
-        sauceOptions.setBrowserVersion(browserVersion);
-        sauceOptions.setPlatformName(platform);
-
-        return sauceOptions;
-    }
+public class DesktopTests extends BaseTest {
 
     @Test()
     public void loginWorks() {
