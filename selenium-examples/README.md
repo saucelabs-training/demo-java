32a59fc9663025d6fbb754122ba5e9666989dfea
titusfortner <titus.fortner@gmail.com>
2023-10-18 13:07:40 -0500

    add demo tests to selenium examples

--- selenium-examples/README.md
+++ selenium-examples/README.md
@@ -2,17 +2,31 @@
 
 This is the default project for all Selenium and Sauce Labs features
 
+There are 3 packages:
+
 * Demo package for seeing Sauce Demo site tests
-* Sauce Features package for demos of executing special Sauce Labs features
-* Selenium Features package for demos of executing Selenium on Sauce Labs
+* Sauce Labs Features package for demos of functionality exclusive to Sauce Labs
+* Selenium Features package for demos of Selenium features on Sauce Labs
 
 It is also to demonstrate how to run tests in parallel with Selenium 4 and JUnit 5. See [pom.xml](pom.xml)
 
-## Execute tests in parallel:
+## Execution options:
+
+First, ensure you are in `selenium-examples` directory
+
 ```bash
 cd selenium-examples
-mvn test -Dsurefire.parallel=20
 ```
 
-
-
+1. Run everything with default parallelization:
+    ```bash
+    mvn clean test
+    ```
+2. Run only demo tests:
+    ```bash
+    mvn clean test -Dtest='com.saucedemo.selenium.demo.*Test'
+    ```
+3. Change parallelization
+    ```bash
+    mvn test -Dsurefire.parallel=20
+    ```
---
+++ selenium-examples/src/test/java/com/saucedemo/selenium/demo/AuthenticationTest.java
@@ -0,0 +1,56 @@
+package com.saucedemo.selenium.demo;
+
+import com.saucedemo.selenium.TestBase;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class AuthenticationTest extends TestBase {
+
+    @BeforeEach
+    public void setup(TestInfo testInfo) {
+        startFirefoxSession(testInfo);
+    }
+
+    @Test
+    public void signInUnsuccessful() {
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.cssSelector("input[data-test='username']")).sendKeys("locked_out_user");
+        driver.findElement(By.cssSelector("input[data-test='password']")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("input[data-test='login-button']")).click();
+
+        WebElement errorElement = driver.findElement(By.cssSelector("[data-test='error']"));
+        Assertions.assertTrue(errorElement.getText().contains("Sorry, this user has been locked out"),
+                "Error Not Found");
+    }
+
+    @Test
+    public void signInSuccessful() {
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.cssSelector("input[data-test='username']")).sendKeys("standard_user");
+        driver.findElement(By.cssSelector("input[data-test='password']")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("input[data-test='login-button']")).click();
+
+        Assertions.assertEquals("https://www.saucedemo.com/inventory.html",
+                driver.getCurrentUrl(),
+                "Login Not Successful");
+    }
+
+    @Test
+    public void logout() throws InterruptedException {
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.cssSelector("input[data-test='username']")).sendKeys("standard_user");
+        driver.findElement(By.cssSelector("input[data-test='password']")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("input[data-test='login-button']")).click();
+        driver.findElement(By.id("react-burger-menu-btn")).click();
+        Thread.sleep(1000);
+
+        driver.findElement(By.id("logout_sidebar_link")).click();
+
+        Assertions.assertEquals("https://www.saucedemo.com/",
+                driver.getCurrentUrl(),
+                "Logout Not Successful");
+    }
+}
