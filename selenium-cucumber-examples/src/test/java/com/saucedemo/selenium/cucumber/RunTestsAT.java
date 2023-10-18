95dcd758ff6005ef4d3a8f79d13da002cdb168d1
titusfortner <titus.fortner@gmail.com>
2023-10-18 14:33:08 -0500

    delete cucumber examples

--- selenium-cucumber-examples/src/test/java/com/saucedemo/selenium/cucumber/RunTestsAT.java
+++
@@ -1,8 +0,0 @@
-package com.saucedemo.selenium.cucumber;
-
-import cucumber.api.CucumberOptions;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
-
-@CucumberOptions(plugin = {"pretty"})
-public class RunTestsAT extends AbstractTestNGCucumberTests{
-}
