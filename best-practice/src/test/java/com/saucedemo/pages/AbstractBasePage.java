b98de91a82b50b1f0135e6880c68bcd02c9e57bb
Diego Molina <diemol@gmail.com>
2024-07-12 22:57:41 +0200

    Updating best practices section to get tests passing

--- best-practice/src/test/java/com/saucedemo/pages/AbstractBasePage.java
+++ best-practice/src/test/java/com/saucedemo/pages/AbstractBasePage.java
@@ -1,43 +1,41 @@
 package com.saucedemo.pages;
 
-import org.openqa.selenium.remote.RemoteWebDriver;
+
+import static com.saucedemo.Constants.WEB_URL;
 
 import java.util.Map;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
-/**
- * All page objects inherit from the base page.
- */
+/** All page objects inherit from the base page. */
 public abstract class AbstractBasePage {
-    protected final RemoteWebDriver driver;
+  protected final RemoteWebDriver driver;
 
-    public RemoteWebDriver getDriver() {
-        return this.driver;
-    }
+  public AbstractBasePage(RemoteWebDriver driver) {
+    this.driver = driver;
+  }
 
-    public AbstractBasePage(RemoteWebDriver driver) {
-        this.driver = driver;
-    }
+  public RemoteWebDriver getDriver() {
+    return this.driver;
+  }
 
-    /**
-     * Executes a visual test.
-     */
-    public final void takeSnapshot() {
-        driver.executeScript("/*@visual.snapshot*/", this.getClass().getSimpleName());
-    }
+  /** Executes a visual test. */
+  public final void takeSnapshot() {
+    driver.executeScript("/*@visual.snapshot*/", this.getClass().getSimpleName());
+  }
 
-    public void visit() {
-        driver.get("https://www.saucedemo.com/" + getPagePart());
-    }
+  public void visit() {
+    driver.get(WEB_URL + getPagePart());
+  }
 
-    public abstract String getPagePart();
+  public abstract String getPagePart();
 
-    /**
-     * Screener uses this JavaScript to provide results of visual snapshot.
-     *
-     * @return Map of visual results
-     */
-    @SuppressWarnings("unchecked")
-    public Map<String, Object> getVisualResults() {
-        return (Map<String, Object>) driver.executeScript("/*@visual.end*/");
-    }
+  /**
+   * Screener uses this JavaScript to provide results of visual snapshot.
+   *
+   * @return Map of visual results
+   */
+  @SuppressWarnings("unchecked")
+  public Map<String, Object> getVisualResults() {
+    return (Map<String, Object>) driver.executeScript("/*@visual.end*/");
+  }
 }
--- best-practice/src/test/java/com/saucedemo/tests/BaseTest.java
+++ best-practice/src/test/java/com/saucedemo/tests/BaseTest.java
@@ -1,5 +1,15 @@
 package com.saucedemo.tests;
 
+import static com.saucedemo.Constants.SAUCE_EU_URL;
+import static com.saucedemo.Constants.SAUCE_US_URL;
+import static com.saucedemo.Constants.region;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Collection;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.TestName;
@@ -10,133 +20,120 @@ import org.junit.runners.Parameterized;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.Collection;
-
-import static com.saucedemo.Constants.*;
-
 public class BaseTest {
-    protected RemoteWebDriver driver;
+  @Parameterized.Parameter public String platform;
 
-    protected String WEB_URL = "https://www.saucedemo.com/";
+  @Parameterized.Parameter(1)
+  public String browserDeviceName;
 
-    @Parameterized.Parameter
-    public String platform;
-    @Parameterized.Parameter(1)
-    public String browserDeviceName;
-    @Parameterized.Parameter(2)
-    public String browserPlatformVersion;
-    @Parameterized.Parameter(3)
-    public String platformName;
+  @Parameterized.Parameter(2)
+  public String browserPlatformVersion;
 
-    @Parameterized.Parameters()
-    public static Collection<Object[]> crossPlatformData() {
-        return Arrays.asList(new Object[][] {
-                { "desktop", "safari", "latest", "macOS 11.00" },
-                { "desktop", "chrome", "latest-1", "macOS 13" },
-                { "desktop", "firefox", "latest", "Windows 11" },
-                { "desktop", "chrome", "latest", "Windows 10" }
-        });
-    }
-    @Rule
-    public TestName name = new TestName();
+  @Parameterized.Parameter(3)
+  public String platformName;
 
-    @Before
-    public void setup() throws MalformedURLException {
+  @Rule public TestName name = new TestName();
+  protected RemoteWebDriver driver;
 
-        System.out.println("BeforeMethod hook");
-        URL url;
-
-        switch (region) {
-            case "us":
-                url = new URL(SAUCE_US_URL);
-                break;
-            case "eu":
-            default:
-                url = new URL(SAUCE_EU_URL);
-                break;
-        }
-
-        boolean isBuildCap = false;
-        MutableCapabilities caps = new MutableCapabilities();
-        MutableCapabilities sauceOptions = new MutableCapabilities();
-
-        switch (platform) {
-            case "desktop":
-                caps.setCapability("browserName", browserDeviceName);
-                caps.setCapability("browserVersion", browserPlatformVersion);
-                caps.setCapability("platformName", platformName);
-                break;
-            case "android":
-                caps.setCapability("platformName", "android");
-                caps.setCapability("appium:automationName", "UiAutomator2");
-                caps.setCapability("browserName", "chrome");
-                caps.setCapability("appium:deviceName", browserDeviceName);
-                caps.setCapability("appium:platformVersion", browserPlatformVersion);
-                break;
-            case "ios":
-                caps.setCapability("platformName", "iOS");
-                caps.setCapability("appium:automationName", "XCuiTest");
-                caps.setCapability("browserName", "safari");
-                caps.setCapability("appium:deviceName", browserDeviceName);
-                caps.setCapability("appium:platformVersion", browserPlatformVersion);
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + platform);
-        }
-
-        sauceOptions.setCapability("username", System.getenv("SAUCE_USERNAME"));
-        sauceOptions.setCapability("accessKey", System.getenv("SAUCE_ACCESS_KEY"));
-        sauceOptions.setCapability("name", name.getMethodName());
-
-        if (!isBuildCap) { //handle build cap
-            LocalDateTime dateTime = LocalDateTime.now();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy-HH");
-            String buildLocal = "sauceDemo-" +dateTime.format(formatter);
-            String buildVal = System.getenv("BUILD_TAG");
-            sauceOptions.setCapability("build", buildVal == null ? buildLocal : buildVal);
-        }
-
-        caps.setCapability("sauce:options", sauceOptions);
-
-        try {
-            driver = new RemoteWebDriver(url, caps);
-
-        } catch (Exception e) {
-            System.out.println("*** Problem to create the driver " + e.getMessage());
-            throw new RuntimeException(e);
-        }
-    }
-
-
-    @Rule
-    public TestRule watcher = new TestWatcher() {
+  @Rule
+  public TestRule watcher =
+      new TestWatcher() {
 
         @Override
         protected void succeeded(Description description) {
-            if(driver != null)
-            {
-                System.out.println("Test Passed!");
-                driver.executeScript("sauce:job-result=passed");
-                driver.quit();
-            }
+          if (driver != null) {
+            System.out.println("Test Passed!");
+            driver.executeScript("sauce:job-result=passed");
+            driver.quit();
+          }
         }
 
         @Override
         public void failed(Throwable e, Description description) {
-            if(driver != null)
-            {
-                System.out.println("Test Failed!");
-                driver.executeScript("sauce:job-result=failed");
-                driver.executeScript("sauce:context=" +e.getMessage());
-                driver.quit();
-            }
+          if (driver != null) {
+            System.out.println("Test Failed!");
+            driver.executeScript("sauce:job-result=failed");
+            driver.executeScript("sauce:context=" + e.getMessage());
+            driver.quit();
+          }
         }
+      };
 
-    };
+  @Parameterized.Parameters()
+  public static Collection<Object[]> crossPlatformData() {
+    return Arrays.asList(
+        new Object[][] {
+          {"desktop", "safari", "latest", "macOS 11.00"},
+          {"desktop", "chrome", "latest-1", "macOS 13"},
+          {"desktop", "firefox", "latest", "Windows 11"},
+          {"desktop", "chrome", "latest", "Windows 10"}
+        });
+  }
 
+  @Before
+  public void setup() throws MalformedURLException {
+
+    System.out.println("BeforeMethod hook");
+    URL url;
+
+    switch (region) {
+      case "us":
+        url = new URL(SAUCE_US_URL);
+        break;
+      case "eu":
+      default:
+        url = new URL(SAUCE_EU_URL);
+        break;
+    }
+
+    boolean isBuildCap = false;
+    MutableCapabilities caps = new MutableCapabilities();
+    MutableCapabilities sauceOptions = new MutableCapabilities();
+
+    switch (platform) {
+      case "desktop":
+        caps.setCapability("browserName", browserDeviceName);
+        caps.setCapability("browserVersion", browserPlatformVersion);
+        caps.setCapability("platformName", platformName);
+        break;
+      case "android":
+        caps.setCapability("platformName", "android");
+        caps.setCapability("appium:automationName", "UiAutomator2");
+        caps.setCapability("browserName", "chrome");
+        caps.setCapability("appium:deviceName", browserDeviceName);
+        caps.setCapability("appium:platformVersion", browserPlatformVersion);
+        break;
+      case "ios":
+        caps.setCapability("platformName", "iOS");
+        caps.setCapability("appium:automationName", "XCuiTest");
+        caps.setCapability("browserName", "safari");
+        caps.setCapability("appium:deviceName", browserDeviceName);
+        caps.setCapability("appium:platformVersion", browserPlatformVersion);
+        break;
+      default:
+        throw new IllegalStateException("Unexpected value: " + platform);
+    }
+
+    sauceOptions.setCapability("username", System.getenv("SAUCE_USERNAME"));
+    sauceOptions.setCapability("accessKey", System.getenv("SAUCE_ACCESS_KEY"));
+    sauceOptions.setCapability("name", name.getMethodName());
+
+    if (!isBuildCap) { // handle build cap
+      LocalDateTime dateTime = LocalDateTime.now();
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy-HH");
+      String buildLocal = "sauceDemo-" + dateTime.format(formatter);
+      String buildVal = System.getenv("BUILD_TAG");
+      sauceOptions.setCapability("build", buildVal == null ? buildLocal : buildVal);
+    }
+
+    caps.setCapability("sauce:options", sauceOptions);
+
+    try {
+      driver = new RemoteWebDriver(url, caps);
+
+    } catch (Exception e) {
+      System.out.println("*** Problem to create the driver " + e.getMessage());
+      throw new RuntimeException(e);
+    }
+  }
 }
