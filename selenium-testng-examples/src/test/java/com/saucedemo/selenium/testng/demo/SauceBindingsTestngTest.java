6ea7a2e7389dc4d95b0a0d9ec1be794b42dc85a1
Diego Molina <diemol@gmail.com>
2024-07-15 01:08:43 +0200

    Removing headless tests and fixing others

--- selenium-testng-examples/src/test/java/com/saucedemo/selenium/testng/demo/SauceBindingsTestngTest.java
+++ selenium-testng-examples/src/test/java/com/saucedemo/selenium/testng/demo/SauceBindingsTestngTest.java
@@ -4,13 +4,12 @@ import com.saucelabs.saucebindings.testng.SauceBaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-/**
- * Example Test for running with the TestNG Sauce Bindings library.
- */
+/** Example Test for running with the TestNG Sauce Bindings library. */
 public class SauceBindingsTestngTest extends SauceBaseTest {
-    @Test
-    public void correctTitle() {
-        getDriver().navigate().to("https://www.saucedemo.com");
-        Assert.assertEquals("Swag Labs", getDriver().getTitle());
-    }
+  @Test
+  public void correctTitle() {
+
+    getDriver().navigate().to("https://www.saucedemo.com");
+    Assert.assertEquals("Swag Labs", getDriver().getTitle());
+  }
 }
